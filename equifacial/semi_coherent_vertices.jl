e1cb9cb41bc5d28cee2b8279b28ce56a18fe46c2
Seth-Kurankyi <seth.kurankyi@gmail.com>
2024-04-07 17:53:41 -0400

    vertex amplitude

--- equifacial/semi_coherent_vertices.jl
+++ equifacial/semi_coherent_vertices.jl
@@ -26,7 +26,7 @@ function cohn_vertex(j,nv)
     vc = vector_coherent4jPh(j*ones(4),nv)
     sol = 0.0im
     @simd for x in 3j:-1.0:0.0
-        w6j = wig6j_matrix([0.0:2j,0.0:2j],j,x,j,j)
+        w6j = wig6j_matrix((0.0:2j,0.0:2j),j,x,j,j)
         w6j =  w6j .* vc 
         sol += (-1)^(2j)*(dim_j(x))* sum(transpose(w6j) .* (w6j^4))
     end
