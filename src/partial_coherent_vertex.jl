cae5d24f6135bc9da438370ee89ae38fc2d52c35
Seth-Kurankyi <seth.kurankyi@gmail.com>
2024-06-13 04:52:46 -0400

    add file for partial coherent vertices

---
+++ src/partial_coherent_vertex.jl
@@ -0,0 +1,15 @@
+"""
+Module `partial_coherent_vertex` compute the coherent vertex amplitude for 'boundary vertices' using matrix contractions.
+"""
+
+module partial_coherent_vertex
+
+# use functions from su2functions 
+include("su2functions.jl")
+using .su2functions
+
+#export the following functions 
+#export  
+
+
+end
