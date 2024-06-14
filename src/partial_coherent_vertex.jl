7e45b71ad1b190a2f8256d89dcf7ce159bbbe059
Seth-Kurankyi <seth.kurankyi@gmail.com>
2024-06-14 08:37:30 -0400

    add partial vertices 1 and 2

--- src/partial_coherent_vertex.jl
+++ src/partial_coherent_vertex.jl
@@ -15,6 +15,7 @@ using .su2functions
 Compute the partial coherent vertex amplitude for fixed virtual spin x: 
     the coherent {4j} vectors are considered as inputs
 """
+#one-valent partial coherent vertex
 # bulk intertwiner chosen to be 1 and boundary intertwiners 2,3,4,5
 function partial_cohnX_vertex1(x,jays,vcs)
     #note the order of the spins, 1,2,3,4,5 labels the edges (tetrahedron)
@@ -45,6 +46,47 @@ function partial_cohnX_vertex1(x,jays,vcs)
     return transpose(sum( (w612) .* transpose(w623 * w634 * w645 * w651) , dims = 1))
 end
 
+"""
+Compute the partial coherent vertex amplitude for given the spins and normal vectors as inputs
+"""
+function partial_cohn_vertex1(jays,nvs)
+    j12,j13,j14,j15,j23,j24,j25,j34,j35,j45 = jays
+    nv1,nv2,nv3,nv4,nv5 = nvs
+    
+    # !!!!!!!   IMPORTANT   !!!!!!!
+    """ The order of the spins are crucial for the matching the coherent vectors and the input normal vectors: 
+        Note that order of the spins should match the intertwiners and also match the order of the normal vectors (inputs)
+    """
+    # The order of the spins are the same as the order of the unit normal vectors as inputs
+    jjs = (j12,j13,j14,j15),(j23,j24,j25,j12),(j34,j35,j13,j23),(j45,j14,j24,j34),(j15,j25,j35,j45)
+
+        
+    # intertwiner ranges 
+    #I1,I2,I3,I4,I5 = intw_range.(jjs)
+
+    # compute the coherent {4j} vector for all 5 boundary edges 
+    vcs = Dict()
+    for i in 1:4 
+        vcs[i] = vector_coherent4jPh(jjs[i],nvs[i])
+    end
+    
+    #vcs = vc1,vc2,vc3,vc4,vc5
+    #length of intertwiner range for I1 
+    lenI1 = length(intw_range(j12,j13,j14,j15))
+    sol = zeros(lenI1)*0.0im 
+    # range of values for the virtual spin 
+    x_range = min( min(j12+j13,j14+j15)+j23, min(j24+j23,j12+j25)+j13, min(j13+j23,j34+j35)+j24,
+                min(j34+j24,j14+j45)+j35 , min(j15+j25,j35+j45)+j14 )
+    
+    @simd for x in x_range:-1:0
+        sol += (-1.0+0.0im)^(sum(jays))*(dim_j(x))*partial_cohnX_vertex1(x,jays,vcs)
+    end
+    return sol
+end
+
+
+
+#two-valent partial coherent vertex
 # bulk intertwiners chosen to be 1,2 and boundary intertwiners 3,4,5
 function partial_cohnX_vertex2(x,jays,vcs)
     #note the order of the spins, 1,2,3,4,5 labels the edges (tetrahedron)
@@ -74,5 +116,47 @@ function partial_cohnX_vertex2(x,jays,vcs)
     # returns a matrix
     return (w612) .* transpose(w623 * w634 * w645 * w651) 
 end
+
+
+
+"""
+Compute the partial coherent vertex amplitude for given the spins and normal vectors as inputs
+"""
+function partial_cohn_vertex2(jays,nvs)
+    j12,j13,j14,j15,j23,j24,j25,j34,j35,j45 = jays
+    nv1,nv2,nv3,nv4,nv5 = nvs
+    
+    # !!!!!!!   IMPORTANT   !!!!!!!
+    """ The order of the spins are crucial for the matching the coherent vectors and the input normal vectors: 
+        Note that order of the spins should match the intertwiners and also match the order of the normal vectors (inputs)
+    """
+    # The order of the spins are the same as the order of the unit normal vectors as inputs
+    jjs = (j12,j13,j14,j15),(j23,j24,j25,j12),(j34,j35,j13,j23),(j45,j14,j24,j34),(j15,j25,j35,j45)
+
+        
+    # intertwiner ranges 
+    #I1,I2,I3,I4,I5 = intw_range.(jjs)
+
+    # compute the coherent {4j} vector for all 5 boundary edges 
+    vcs = Dict()
+    for i in 1:4 
+        vcs[i] = vector_coherent4jPh(jjs[i],nvs[i])
+    end
+    
+    #vcs = vc1,vc2,vc3,vc4,vc5
+    #length of intertwiner range for I1 
+    lenI1,lenI2 = length(intw_range(j12,j13,j14,j15)), length(intw_range(j23,j24,j25,j12))
+    sol = zeros(lenI1,lenI2)*0.0im 
+    # range of values for the virtual spin 
+    x_range = min( min(j12+j13,j14+j15)+j23, min(j24+j23,j12+j25)+j13, min(j13+j23,j34+j35)+j24,
+                min(j34+j24,j14+j45)+j35 , min(j15+j25,j35+j45)+j14 )
+    
+    @simd for x in x_range:-1:0
+        sol += (-1.0+0.0im)^(sum(jays))*(dim_j(x))*partial_cohnX_vertex2(x,jays,vcs)
+    end
+    return sol
+end
+
+
 
 end
