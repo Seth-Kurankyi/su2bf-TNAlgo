41c35d7bafd968d72e66d4fabc38a5f913f7ba21
Seth-Kurankyi <seth.kurankyi@gmail.com>
2024-06-19 05:59:04 -0400

    add to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,15 @@
 #add test soon 
+
+include("../src/coherent_vertex.jl")
+
+using .coherent_vertex
+
+# face normal vectors for an equilateral tetrahedron
+nn= [[0.0, 0.0, 1.0], [0.0, 2sqrt(2)/3, -1/3], 
+    [sqrt(2/3), -sqrt(2)/3, -1/3], 
+    [-sqrt(2/3), -sqrt(2)/3, -1/3]];
+
+
+
+@time cohn_vertex1(ones(10),[nn,nn,nn,nn,nn])
+
